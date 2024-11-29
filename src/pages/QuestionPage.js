61dc516743920cc9723bfdb48f43f9fd6b1d78da
Ventura <joao.ventura1@alunos.sc.senac.br>
2024-11-28 21:59:02 -0300

    feat: Add basic progression

--- src/pages/QuestionPage.js
+++ src/pages/QuestionPage.js
@@ -1,12 +1,49 @@
-import React, { useState, useEffect } from 'react'; // Hooks do React para estados e efeitos colaterais
-import axios from '../services/AxiosConfig.js'; // Instância configurada do axios
+import React, { useState, useEffect } from "react"; // Hooks do React para estados e efeitos colaterais
+import axios from "../services/AxiosConfig.js"; // Instância configurada do axios
+import "./QuestionPage.css";
 
 const QuestionPage = () => {
+  const listTrail = [
+    "R$ 1.000,00",
+    "R$ 1.000,00",
+    "R$ 1.000,00",
+    "R$ 1.000,00",
+    "R$ 1.000,00",
+    "R$ 10.000,00",
+    "R$ 10.000,00",
+    "R$ 10.000,00",
+    "R$ 10.000,00",
+    "R$ 10.000,00",
+    "R$ 100.000,00",
+    "R$ 100.000,00",
+    "R$ 100.000,00",
+    "R$ 100.000,00",
+    "R$ 100.000,00",
+  ];
+
+  const [selectedOption, setSelectedOption] = useState(null);
+
+  // PERGUNTA
   const [question, setQuestion] = useState(null); // Armazena a pergunta atual
+  // ALTERNATIVAS
   const [options, setOptions] = useState([]); // Armazena as opções de resposta
+  // PONTUACAO
   const [score, setScore] = useState(0); // Armazena a pontuação do jogador
-  const [skips, setSkips] = useState(2); // Armazena o número de pulos restantes
+
+  // NIVEL
+  const [level, setLevel] = useState("FACIL");
+
+  // NUMERO DA QUESTAO
+  const [questionNumber, setQuestionNumber] = useState(0);
+
   const [gameOver, setGameOver] = useState(false); // Indica se o jogo terminou
+  const [gameStarted, setGameStarted] = useState(false);
+
+  // Transição
+  const [isTransitioning, setIsTransitioning] = useState(false);
+  const [transitionNumber, setTransitionNumber] = useState(3);
+
+  const [error, setError] = useState("");
 
   // Busca uma pergunta do backend ao carregar a página
   useEffect(() => {
@@ -15,35 +52,48 @@ const QuestionPage = () => {
 
   // Função para buscar uma pergunta do backend
   const fetchQuestion = () => {
-    axios.get('/pergunta/nivel/1') // Altere o nível conforme o progresso do jogador
+    verifyLevel();
+
+    axios
+      .get(`/perguntas/${level}`) // Altere o nível conforme o progresso do jogador
       .then((response) => {
-        setQuestion(response.data.question); // Define a pergunta atual
-        setOptions(response.data.options); // Define as opções de resposta
+        if (questionNumber >= 15) {
+          setQuestion(response.data[0].pergunta); // Define a pergunta atual
+          setOptions(response.data[0].alternativas); // Define as opções de resposta
+        } else if (questionNumber >= 10) {
+          setQuestion(response.data[questionNumber - 10].pergunta); // Define a pergunta atual
+          setOptions(response.data[questionNumber - 10].alternativas); // Define as opções de resposta
+        } else {
+          setQuestion(response.data[questionNumber].pergunta); // Define a pergunta atual
+          setOptions(response.data[questionNumber].alternativas); // Define as opções de resposta
+        }
       })
       .catch((error) => {
-        console.error('Erro ao buscar pergunta:', error);
+        setError("Erro ao buscar pergunta:", error);
+        console.error("Erro ao buscar pergunta:", error);
       });
   };
 
   // Função chamada ao jogador responder a uma pergunta
-  const handleAnswer = (isCorrect) => {
-    if (isCorrect) {
-      setScore(score + question.prize); // Atualiza o montante
-      alert(`Resposta correta! Você ganhou R$ ${question.prize}.`);
-      fetchQuestion(); // Carrega a próxima pergunta
-    } else {
-      alert('Resposta errada! Você perdeu tudo.');
-      setGameOver(true); // Termina o jogo
-    }
-  };
+  const handleAnswer = () => {
+    if (selectedOption.correta) {
+      if (questionNumber < 5) {
+        setScore(score + 1000); // Atualiza o montante
+      } else if (questionNumber < 10) {
+        setScore(score + 10000); // Atualiza o montante
+      } else if (questionNumber < 15) {
+        setScore(score + 10000); // Atualiza o montante
+      } else {
+        setScore(score + 10000); // Atualiza o montante
+      }
 
-  // Função chamada ao jogador pular uma pergunta
-  const handleSkip = () => {
-    if (skips > 0) {
-      setSkips(skips - 1); // Reduz o número de pulos restantes
-      fetchQuestion(); // Carrega uma nova pergunta
+      setQuestionNumber(questionNumber + 1);
+      alert(`Resposta correta! Você ganhou R$ ${score}`);
+      setSelectedOption(null);
+      handleTransition(true);
     } else {
-      alert('Você não tem mais pulos disponíveis!');
+      alert("Resposta errada! Você perdeu tudo.");
+      setGameOver(true); // Termina o jogo
     }
   };
 
@@ -53,36 +103,141 @@ const QuestionPage = () => {
     setGameOver(true); // Termina o jogo
   };
 
+  const handleTransition = (shouldFetchQuestion) => {
+    setGameStarted(true);
+    setIsTransitioning(true);
+    setTransitionNumber(3);
+
+    const interval = setInterval(() => {
+      setTransitionNumber((previousNumber) => {
+        if (previousNumber === 0) {
+          clearInterval(interval);
+          setIsTransitioning(false);
+          return previousNumber;
+        }
+        return previousNumber - 1;
+      });
+    }, 1000);
+
+    if (shouldFetchQuestion) {
+      fetchQuestion();
+    }
+  };
+
+  const verifyLevel = () => {
+    if (score > 555000) {
+      setLevel("AVANCADO");
+    } else if (score > 55000) {
+      setLevel("DIFICIL");
+    } else if (score > 5000) {
+      setLevel("MODERADO");
+    } else {
+      setLevel("FACIL");
+    }
+  };
+
+  const handleSelect = (option) => {
+    setSelectedOption(option); // Atualiza o estado com a opção selecionada
+  };
+
   return (
-    <div>
-      {gameOver ? ( // Verifica se o jogo terminou
-        <div>
-          <h1>Fim de Jogo!</h1>
-          <h2>Montante Final: R$ {score}</h2>
-        </div>
-      ) : (
-        <>
-          {question ? ( // Exibe a pergunta quando carregada
-            <>
-              <h1>{question.text}</h1>
-              <div>
-                {/* Renderiza as opções de resposta */}
-                {options.map((option, index) => (
-                  <button key={index} onClick={() => handleAnswer(option.isCorrect)}>
-                    {option.text}
+    <div className="questionpage-container">
+      <div className="questionpage-photo-container">
+        {!gameStarted ? (
+          <button
+            className="start-button"
+            onClick={() => handleTransition(true)}
+            disabled={isTransitioning}
+          >
+            Iniciar
+          </button>
+        ) : isTransitioning ? (
+          <h1 className="transition-number">{transitionNumber}</h1>
+        ) : (
+          <div className="question-container">
+            <h1 className="question-title">{question}</h1>
+            <div className="options-container">
+              {options.map((option) => (
+                <div key={option.id} className="option-wrapper">
+                  <button
+                    className={`option-button ${
+                      selectedOption && selectedOption.id === option.id
+                        ? "selected"
+                        : ""
+                    }`}
+                    onClick={() => handleSelect(option)}
+                    type="select"
+                  >
+                    {option.descricao}
                   </button>
-                ))}
-              </div>
-              {/* Botões para pular ou parar o jogo */}
-              <button onClick={handleSkip}>Pular (Restam {skips})</button>
-              <button onClick={handleStop}>Parar e Sair com o Montante</button>
-              <h2>Montante Atual: R$ {score}</h2>
-            </>
-          ) : (
-            <p>Carregando pergunta...</p> // Mensagem enquanto a pergunta carrega
-          )}
-        </>
-      )}
+                </div>
+              ))}
+            </div>
+            <div className="actions-container">
+              <button className="action-button stop-button">PARAR</button>
+              <button
+                className="action-button confirm-button"
+                onClick={() => handleAnswer()}
+              >
+                CONFIRMAR ALTERNATIVA
+              </button>
+            </div>
+          </div>
+        )}
+      </div>
+      <div className="questionpage-trilha-container">
+        <div className="questionpage-trilha-mini-container">
+          <ul className="list-trilha list-1milhao">
+            {questionNumber >= 15 ? (
+              <li className="li-1milhao">R$ 1.000.000,00</li>
+            ) : (
+              <li>R$ 1.000.000,00</li>
+            )}
+          </ul>
+          <ul className="list-trilha list-100mil">
+            {listTrail.map((item, index) => {
+              if (index > 9) {
+                if (index === questionNumber) {
+                  return <li className="current-question-number">{item}</li>;
+                }
+                return <li>{item}</li>;
+              }
+            })}
+            <li>Nível 3</li>
+          </ul>
+          <ul className="list-trilha list-10mil">
+            {listTrail.map((item, index) => {
+              if (index > 4 && index < 10) {
+                if (index === questionNumber) {
+                  return <li className="current-question-number">{item}</li>;
+                }
+                return <li>{item}</li>;
+              }
+            })}
+            <li>Nível 2</li>
+          </ul>
+          <ul className="list-trilha list-1mil" reversed>
+            {listTrail.map((item, index) => {
+              if (index < 5) {
+                if (index === questionNumber) {
+                  return <li className="current-question-number">{item}</li>;
+                }
+                return <li>{item}</li>;
+              }
+            })}
+            <li>Nível 1</li>
+          </ul>
+          <ul className="list-trilha">
+            <li>
+              Você tem:{" "}
+              {new Intl.NumberFormat("pt-BR", {
+                style: "currency",
+                currency: "BRL",
+              }).format(score)}
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
--- src/services/AxiosConfig.js
+++ src/services/AxiosConfig.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Cria uma instância do axios com a URL base da API
 const instance = axios.create({
-  baseURL: 'http://localhost:3000/api', // Substitua pelo URL do seu backend
+  baseURL: 'http://localhost:8080/', // Substitua pelo URL do seu backend
 });
 
 export default instance;
